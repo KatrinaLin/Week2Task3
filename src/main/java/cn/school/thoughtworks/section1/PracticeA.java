a5378cc5f36e16269f89db897b2d15a48c3a31ca
Katrina <zhaoyanlin2016@gmail.com>
2018-03-30 23:10:56 -0700

    complete PracticeC in Section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -5,11 +5,11 @@ import java.util.stream.Collectors;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
         if (collection1 == null || collection2 == null) {
             return null;
         }
 
+        //选出1集合中与2集合中相同的元素
         return collection1
                 .stream()
                 .filter(i -> collection2.contains(i))
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -2,10 +2,18 @@ package cn.school.thoughtworks.section1;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        if (collection1 == null || collection2 == null) {
+            return null;
+        }
+
+        //选出1集合中跟2对象中value属性中的元素相同的元素
+        return collection1
+                .stream()
+                .filter(i -> collection2.get("value").contains(i))
+                .collect(Collectors.toList());
     }
 }
