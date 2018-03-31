025e09c61942b39be1be397de834c14dc11fb473
Katrina <zhaoyanlin2016@gmail.com>
2018-03-30 22:46:10 -0700

    complete PracticeA in section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        if (collection1 == null || collection2 == null) {
+            return null;
+        }
+
+        return collection1
+                .stream()
+                .filter(i -> collection2.contains(i))
+                .collect(Collectors.toList());
     }
 }
