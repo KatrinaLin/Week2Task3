35dc73cd6507fec329709d8100ff53642ce23e86
Katrina <zhaoyanlin2016@gmail.com>
2018-03-30 23:22:12 -0700

    complete PracticeB in Section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,30 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        if (collection1 == null || collection2 == null) {
+            return null;
+        }
+
+        return collection1
+                .stream()
+                .filter(i -> (isContainElement(i, collection2)))
+                .collect(Collectors.toList());
+
+    }
+
+    boolean isContainElement(String target, List<List<String>> collection2) {
+
+        for (List<String> l : collection2) {
+            if (l.contains(target)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
