a25b3fcce117adbb89e432d583aa6cea6b06ec24
Katrina <zhaoyanlin2016@gmail.com>
2018-03-31 20:00:29 -0700

    complete PracticeA in Section3 using stream API and Map.computeIfPresent function.

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,5 +1,6 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,11 @@ public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
 
-        return null;
+        List<String> list = object.get("value");
+
+        // if the element in the list exists in the map, then reduce its value by 1
+        list.stream().forEach(x -> collectionA.computeIfPresent(x, (k, v) -> v - 1));
+
+        return collectionA;
     }
 }
