6803446c698f79807c856b63ffccc3408a1e0f88
Katrina <zhaoyanlin2016@gmail.com>
2018-03-31 20:05:06 -0700

    complete PracticeB in Section3 using stream API and Map.computeIfPresent function.

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,11 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> list = object.get("value");
 
-        return null;
+        // 选出collectionA中key跟collection2中value对应的list中的元素相同的元素,把他们的count满3减1
+        list.stream().forEach(x -> collectionA.computeIfPresent(x, (k, v) -> v - v / 3));
+
+        return collectionA;
     }
 }
